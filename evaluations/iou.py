5bceef949c516000c56d165fc85c4c67f0c6731f
UNCLE-KIZARU <parsaaskari1380@gmail.com>
2024-06-02 23:52:29 +0330

    initial commit

--- evaluations/iou.py
+++ evaluations/iou.py
@@ -4,26 +4,20 @@ def calculate_iou(predictions,main_box,form="circle"):
     predictions shape (batch_size,4) [x1 , y1 ,x2 ,y2] for triangle and [x1 , y1 ,h1 ,w1] for circle
     main box shape (batch_size , 4) [x1 , y1 ,x2 ,y2] for triangle and [x1 , y1 ,h1 ,w1] for circle
     """
-    if(form=="triangle"):
-        x1_p=predictions[:,0:1]
-        y1_p=predictions[:,1:2]
-        x2_p=predictions[:,2:3]
-        y2_p=predictions[:,3:4]
+    if(type(predictions)==list):
+        predictions=torch.tensor(predictions)
+    if(type(main_box)==list):
+        main_box=torch.tensor(main_box)
+   
+    x1_p=predictions[...,0:1] - predictions[...,2:3] / 2
+    y1_p=predictions[...,1:2] - predictions[...,3:4] / 2
+    x2_p=predictions[...,0:1] + predictions[...,2:3] / 2
+    y2_p=predictions[...,1:2] + predictions[...,3:4] / 2
 
-        x1_m=main_box[:,0:1]
-        y1_m=main_box[:,1:2]
-        x2_m=main_box[:,2:3]
-        y2_m=main_box[:,3:4]
-    else:
-        x1_p=predictions[:,0:1] - predictions[:,2:3] / 2
-        y1_p=predictions[:,1:2] - predictions[:,3:4] / 2
-        x2_p=predictions[:,0:1] + predictions[:,2:3] / 2
-        y2_p=predictions[:,1:2] + predictions[:,3:4] / 2
-
-        x1_m=main_box[:,0:1] - main_box[:,2:3] / 2
-        y1_m=main_box[:,1:2] - main_box[:,3:4] / 2
-        x2_m=main_box[:,0:1] + main_box[:,2:3] / 2
-        y2_m=main_box[:,1:2] + main_box[:,3:4] / 2
+    x1_m=main_box[...,0:1] - main_box[...,2:3] / 2
+    y1_m=main_box[...,1:2] - main_box[...,3:4] / 2
+    x2_m=main_box[...,0:1] + main_box[...,2:3] / 2
+    y2_m=main_box[...,1:2] + main_box[...,3:4] / 2
     # calculate intersection
     # (x1_i,y1_i)=max (x1_p , x1_m),max (y1_p , y1_m)
     # (x2_i,y2_i)=min (x2_p , x2_m),min (y2_p , y2_m)
@@ -32,9 +26,11 @@ def calculate_iou(predictions,main_box,form="circle"):
     x2_i=torch.min(x2_p,x2_m)
     y2_i=torch.min(y2_p,y2_m)
 
-    intersection = torch.clamp((x1_i-x2_i),min=0)*torch.clamp((y1_i-y2_i),min=0)
-
+    intersection = torch.clamp((x2_i-x1_i),min=0)*torch.clamp((y2_i-y1_i),min=0)
+    # print(x1_i,x2_i,y1_i,y2_i)
+    
     union = (abs(x1_p - x2_p) * abs(y1_p - y2_p)) + (abs(x1_m - x2_m) * abs(y1_m - y2_m)) - intersection
+    print(intersection / (union + 1e-6))
     return intersection / (union + 1e-6)
 
 
---
+++ evaluations/non-max-suppression.py
@@ -0,0 +1,27 @@
+from iou import  calculate_iou
+import numpy as np
+def non_max_suppression(boxes,prob_thresh,iou_thresh):
+    """
+        boxes : bounding boxes . [object_prob,class,x,y,w,h]
+        iou_thresh : a threshold for iou score . if > then we will delete the box
+        prob_thresh : a threshold for proboblity of an object being captured bo the box. if < then we will drop the box
+    """
+    boxes=sorted(boxes,key=lambda x : x[0],reverse=True) #sort the boxes based on their object_prob
+    boxes=np.array(boxes)  
+    boxes=boxes[boxes[:,0]>=prob_thresh].tolist() # drop the ones that have obejct_prob < prob_thresh
+    
+    trimed_boxes=[]
+    while boxes!=[]:
+        refrence=boxes.pop(0)
+        new_boxes=[box for box in boxes if calculate_iou(predictions=box[2:],main_box=refrence[2:]) <= iou_thresh or box[1]!=refrence[1]]
+        trimed_boxes.append(refrence)
+        boxes=new_boxes
+    return trimed_boxes
+
+boxes=[[0.4,1,0.2,0,5,1,2],[0.9,1,0.1,0,5,1,2],[0.9,0,0.2,0,5,1,2]]
+iou_thresh=0.5
+prob_th=0.4
+print(non_max_suppression(boxes,prob_th,iou_thresh))
+
+
+
